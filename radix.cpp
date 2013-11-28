5fb536875111c422f23a9001a4b646a1034410c2
Aaron Josephs <ajoseph4@binghamton.edu>
2013-11-28 01:30:24 -0500

    added iterator version of radix sort, might be slightly faster than other version

--- radix.cpp
+++ radix.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 #include <chrono>
 #include <algorithm>
-//#include "cxx-prettyprint/prettyprint.hpp"
+#include <iterator>
+#include "../cxx-prettyprint/prettyprint.hpp"
 #include <assert.h>
 
 using uint = unsigned int; 
@@ -35,6 +36,36 @@ void radix_sort(std::vector<uint> & list, const uint & max_digits=8) {
 }
 //could defintely optimize more by not copying back to the original vector 
 //every time 
+//with iterators, to fit stdlib pattern
+template <typename Iter>
+void radix_sort(Iter begin, Iter end, const uint & max_digits=8) {
+    //default of 8 is the max number of hex digits in an unsigned int 
+    std::array<std::vector<uint>,16> buckets;
+    for (auto & v : buckets) v.reserve(std::distance(begin,end)/16 * 2); 
+    //reserve double the avg bucket size 
+    //go through each digit
+    for (int i = 0; i < max_digits; ++i) { 
+        //put into buckets based on lsd
+        //for (const auto & num : list) {
+        for (auto temp = begin; temp != end; ++temp) {
+            //uint num = *temp;
+            size_t shift = i * 4;
+            size_t index = (*temp >> shift) & 0xF; 
+            buckets[index].emplace_back(*temp);
+        }
+        //put back into vector
+        //uint index = 0;
+        auto temp = begin;
+        for (auto & v : buckets) {
+            for (const auto & num : v ) {
+                //list[index] = num;
+                *temp= num;
+                ++temp;
+            }
+            v.clear();
+        }
+    }
+}
 
 //not nearly as fast as hex
 void radix_sort_binary(std::vector<uint> & list) {
@@ -69,7 +100,7 @@ int main() {
     std::vector<uint> v1;
     std::random_device rd;
     std::default_random_engine eng(rd());
-    std::uniform_int_distribution<uint> dist(1,0xFFFF);
+    std::uniform_int_distribution<uint> dist(1,0xFFFFFFFF);
     for (int i = 0; i < 5000000; ++i) {
         v1.push_back(dist(eng));
     }
@@ -79,7 +110,8 @@ int main() {
 
     //test with radix_sort
     auto start = std::chrono::system_clock::now();
-    radix_sort(v1);
+    radix_sort(v1.begin(),v1.end());
+    //radix_sort(v1);
     auto end = std::chrono::system_clock::now();
     auto radix_time = (end-start).count();
     //
