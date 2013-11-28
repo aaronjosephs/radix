f57a94234e1c70e12c2dae6372899c4220cbfba9
Aaron Josephs <ajoseph4@binghamton.edu>
2013-11-28 00:14:43 -0500

    added stable sort comparison

--- radix.cpp
+++ radix.cpp
@@ -65,32 +65,51 @@ void radix_sort_binary(std::vector<uint> & list) {
 }
 
 int main() {
-    std::vector<uint> v;
+    //create arrays
+    std::vector<uint> v1;
     std::random_device rd;
     std::default_random_engine eng(rd());
-    std::uniform_int_distribution<uint> dist(1,0xFFFFFFFF);
+    std::uniform_int_distribution<uint> dist(1,0xFFFF);
     for (int i = 0; i < 5000000; ++i) {
-        v.push_back(dist(eng));
+        v1.push_back(dist(eng));
     }
-    auto v1 = v;
-    //std::cout << "Unsorted\n" << std::endl;
-    //std::cout << v << std::endl;
+    auto v2 = v1;
+    auto v3 = v1;
+    //
+
+    //test with radix_sort
     auto start = std::chrono::system_clock::now();
-    radix_sort(v);
+    radix_sort(v1);
     auto end = std::chrono::system_clock::now();
     auto radix_time = (end-start).count();
-
+    //
+    
+    //test with sort
     start = std::chrono::system_clock::now();
-    std::sort(v1.begin(),v1.end());
+    std::sort(v2.begin(),v2.end());
     end = std::chrono::system_clock::now();
     auto std_sort_time = (end-start).count();
-    for (size_t i = 0; i < v.size(); ++i) {
-        assert(v[i]==v1[i]);
+    //
+    
+    //test with stable_sort
+    start = std::chrono::system_clock::now();
+    std::sort(v3.begin(),v3.end());
+    end = std::chrono::system_clock::now();
+    auto std_stable_sort_time = (end-start).count();
+    //
+    
+    //check successful sort
+    for (size_t i = 0; i < v1.size(); ++i) {
+        assert(v2[i]==v1[i]);
     }
+
+    //prevent sorts from being optimized out
     std::cout << v1.back() << std::endl;
-    std::cout << v.back() << std::endl;
+    std::cout << v2.back() << std::endl;
+    std::cout << v3.back() << std::endl;
 
     std::cout << "radix took: " << radix_time << std::endl;
     std::cout << "std::sort took: " << std_sort_time << std::endl;
+    std::cout << "std::stable_sort took: " << std_stable_sort_time << std::endl;
     return 0;
 }
